e211b762c7298b6de6413fcce51121bdb5691716
JingMission <768902677@qq.com>
2026-06-23 16:07:23 +0800

    XssFilter与CacheRequestFilter

--- pmhub-gateway/src/main/java/com/laigeoffer/pmhub/gateway/filter/CacheRequestFilter.java
+++ pmhub-gateway/src/main/java/com/laigeoffer/pmhub/gateway/filter/CacheRequestFilter.java
@@ -15,7 +15,8 @@ import java.util.List;
 
 /**
  * 获取body请求数据（解决流不能重复读取问题）
- * 
+ * InputStream（输入流）的设计遵循**“单向流动”**的原则。当你从一个流中读取数据时，内部的读取指针（或位置标记）会不断向后移动。当数据被读取完毕（到达流末尾，返回 -1）后，指针就停在了末尾。
+ * 再次调用read()时，会直接返回-1，无法获取其他数据
  * @author canghe
  */
 @Component
@@ -32,15 +33,19 @@ public class CacheRequestFilter extends AbstractGatewayFilterFactory<CacheReques
         return "CacheRequestFilter";
     }
 
+    // apply()是过滤器的核心装配方法，创建了实际的过滤器实例
     @Override
     public GatewayFilter apply(Config config)
     {
+        // 实例化一个自定义的过滤器，功能为缓存请求体
         CacheRequestGatewayFilter cacheRequestGatewayFilter = new CacheRequestGatewayFilter();
+        // 获取配置中的order，如果为空则使用默认值
         Integer order = config.getOrder();
         if (order == null)
         {
             return cacheRequestGatewayFilter;
         }
+        // 配置了order值后，对原始过滤器进行包装
         return new OrderedGatewayFilter(cacheRequestGatewayFilter, order);
     }
 
@@ -49,17 +54,21 @@ public class CacheRequestFilter extends AbstractGatewayFilterFactory<CacheReques
         @Override
         public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain)
         {
-            // GET DELETE 不过滤
+            // GET DELETE 不过滤(因为这两类请求没有请求体)
             HttpMethod method = exchange.getRequest().getMethod();
             if (method == null || method == HttpMethod.GET || method == HttpMethod.DELETE)
             {
+                // 放行到下一个过滤器
                 return chain.filter(exchange);
             }
+            // 生成了新的请求体对象然后判断是否缓存过，缓存过就放行，没缓存过就把新的请求对象放到exchange中
             return ServerWebExchangeUtils.cacheRequestBodyAndRequest(exchange, (serverHttpRequest) -> {
+                // 如果传入的serverHttpRequest和原始请求是一个对象，说明没有包装(请求体为空或者已经被缓存过)，放行exchange到下一个过滤器
                 if (serverHttpRequest == exchange.getRequest())
                 {
                     return chain.filter(exchange);
                 }
+                // 如果生成了新的请求对象，必须将这个新的请求对象设置到 ServerWebExchange 中，然后放行这个被修改过的新 exchange，后续的过滤器才能拿到可重复读的请求体。
                 return chain.filter(exchange.mutate().request(serverHttpRequest).build());
             });
         }
--- pmhub-gateway/src/main/java/com/laigeoffer/pmhub/gateway/filter/XssFilter.java
+++ pmhub-gateway/src/main/java/com/laigeoffer/pmhub/gateway/filter/XssFilter.java
@@ -21,17 +21,20 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 /**
  * 跨站脚本过滤器
  *
  * @author canghe
+ * @description 主要功能是防御跨站脚本攻击(XSS),拦截请求，读取并清洗请求体中的恶意脚本，实现接口的安全防护
  */
 @Component
+//条件注解：只有配置文件中 security.xss.enabled=true 时，当前类 / 方法才会注册为 Spring Bean。
 @ConditionalOnProperty(value = "security.xss.enabled", havingValue = "true")
 public class XssFilter implements GlobalFilter, Ordered
 {
-    // 跨站脚本的 xss 配置，nacos自行添加
+    // 跨站脚本的 xss 配置，nacos自行添加，已在Properties中创建过配置类了，直接注入即可
     @Autowired
     private XssProperties xss;
 
@@ -44,23 +47,24 @@ public class XssFilter implements GlobalFilter, Ordered
         {
             return chain.filter(exchange);
         }
-        // GET DELETE 不过滤
+        // GET DELETE 不过滤(因为get和delete请求通常没有请求体)
         HttpMethod method = request.getMethod();
         if (method == null || method == HttpMethod.GET || method == HttpMethod.DELETE)
         {
             return chain.filter(exchange);
         }
-        // 非json类型，不过滤
+        // 非json类型，不过滤(防止图片和二进制文件被破坏)
         if (!isJsonRequest(exchange))
         {
             return chain.filter(exchange);
         }
-        // excludeUrls 不过滤
+        // excludeUrls 不过滤(由于公告模块本身包含大量的HTML标签，如果过滤页面展示时无法正常渲染排版，全部显示原始代码文本)
         String url = request.getURI().getPath();
         if (StringUtils.matches(url, xss.getExcludeUrls()))
         {
             return chain.filter(exchange);
         }
+        //
         ServerHttpRequestDecorator httpRequestDecorator = requestDecorator(exchange);
         return chain.filter(exchange.mutate().request(httpRequestDecorator).build());
 
@@ -68,23 +72,33 @@ public class XssFilter implements GlobalFilter, Ordered
 
     private ServerHttpRequestDecorator requestDecorator(ServerWebExchange exchange)
     {
+        //重写ServerHttpRequestDecorator的getBody方法以及getHeaders方法，实现自定义
         ServerHttpRequestDecorator serverHttpRequestDecorator = new ServerHttpRequestDecorator(exchange.getRequest())
         {
+            /**
+             * 处理HTTP请求体并防御XSS攻击的典型实现
+             * @return
+             */
             @Override
             public Flux<DataBuffer> getBody()
             {
+                //在WebFlux中，请求体是以 DataBuffer（数据缓冲区）的形式以流（Flux）的方式异步传递的。
                 Flux<DataBuffer> body = super.getBody();
                 return body.buffer().map(dataBuffers -> {
                     DataBufferFactory dataBufferFactory = new DefaultDataBufferFactory();
                     DataBuffer join = dataBufferFactory.join(dataBuffers);
                     byte[] content = new byte[join.readableByteCount()];
+                    // 将DataBuffer中的数据读取到字节数组中
                     join.read(content);
+                    // 释放掉内存
                     DataBufferUtils.release(join);
+                    // 转成UTF-8字符串
                     String bodyStr = new String(content, StandardCharsets.UTF_8);
-                    // 防xss攻击过滤
+                    // 防xss攻击过滤，对字符串进行清洗，转义
                     bodyStr = EscapeUtil.clean(bodyStr);
                     // 转成字节
                     byte[] bytes = bodyStr.getBytes(StandardCharsets.UTF_8);
+                    // 使用 NettyDataBufferFactory 分配一个新的 DataBuffer，将字节数组写入，并返回。这样下游处理拿到的就是被XSS过滤后的安全请求体。
                     NettyDataBufferFactory nettyDataBufferFactory = new NettyDataBufferFactory(ByteBufAllocator.DEFAULT);
                     DataBuffer buffer = nettyDataBufferFactory.allocateBuffer(bytes.length);
                     buffer.write(bytes);
@@ -92,13 +106,20 @@ public class XssFilter implements GlobalFilter, Ordered
                 });
             }
 
+
+            /**
+             * 其核心目的是将请求的传输编码方式从传统的Content-Length（固定长度）模式转换为Transfer-Encoding: chunked（分块传输）模式。
+             * @return
+             */
             @Override
             public HttpHeaders getHeaders()
             {
                 HttpHeaders httpHeaders = new HttpHeaders();
+                // 获取原始请求的所有HTTP头信息，全部复制到新创建的对象中
                 httpHeaders.putAll(super.getHeaders());
-                // 由于修改了请求体的body，导致content-length长度不确定，因此需要删除原先的content-length
+                // 由于getBody()中修改了请求体的body，导致content-length长度不确定，因此需要删除原先的content-length
                 httpHeaders.remove(HttpHeaders.CONTENT_LENGTH);
+                // 添加（或覆盖）Transfer-Encoding头，将其值设置为chunked，告诉服务端本次请求采用分块传输编码。
                 httpHeaders.set(HttpHeaders.TRANSFER_ENCODING, "chunked");
                 return httpHeaders;
             }
@@ -118,6 +139,7 @@ public class XssFilter implements GlobalFilter, Ordered
         return StringUtils.startsWithIgnoreCase(header, MediaType.APPLICATION_JSON_VALUE);
     }
 
+    //优先级相较于AuthFilter低，全局过滤器中第二个执行
     @Override
     public int getOrder()
     {
